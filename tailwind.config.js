034bca78cc653084603fa9bcb4bea0e66df5b3c7
wl39 <iwg6852@gmail.com>
2024-03-18 15:03:11 +0000

    Resolved the issue: Warnings #68

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
       minHeight: {
-        'screen-50': '50vh',
-      },margin: {
-        '6.25': '25px',
+        "screen-50": "50vh",
+      },
+      margin: {
+        6.25: "25px",
       },
     },
   },
@@ -15,5 +16,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
-
+};
